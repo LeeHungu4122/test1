c6d9eaa61a72f0f2ab92e47f90040da3edc5a32e
LeeHungu4122 <ilopio@naver.com>
2019-09-22 16:48:44 +0900

    doAdd 서비스 구현

--- src/main/java/com/sbs/starter/service/ArticleService.java
+++ src/main/java/com/sbs/starter/service/ArticleService.java
@@ -2,9 +2,12 @@ package com.sbs.starter.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.sbs.starter.dto.Article;
 
 public interface ArticleService {
 	public List<Article> getList();
+
+	public void add(Map<String, Object> param);
 }
